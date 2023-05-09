9d46806b237ee38284b40843e0094bbc801ea366
Mathieu Jouhet <mat.jouhet@gmail.com>
2023-05-09 11:36:08 +0200

    update

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,7 +1,6 @@
-import {button, ButtonVariants} from "./button.css";
-import {ReactNode} from "react";
+import { button, ButtonVariants } from "./Button.css";
+import { ReactNode } from "react";
 
-export const Button = ({color, children}: ButtonVariants & {children: ReactNode}) => {
-  return <button className={button({color})}>{children}</button>
-
-}
+export const Button = ({ color, children }: ButtonVariants & { children: ReactNode }) => {
+  return <button className={button({ color })}>{children}</button>;
+};
